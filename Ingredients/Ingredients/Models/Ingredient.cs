f34f8f914e41d83d155c2311775cd1f13382e58b
Khechar Boorla <khechar@gmail.com>
2015-11-27 17:39:48 -0600

    populate source

---
+++ Ingredients/Ingredients/Models/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+
+namespace Ingredients.Models {
+    public class Ingredient {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public string Units { get; set; }
+        public string Sku { get; set; }
+        public string Supplier { get; set; }
+    }
+
+    public class IngredientDBContext : DbContext {
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
